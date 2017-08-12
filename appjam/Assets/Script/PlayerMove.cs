3dd836c7a7e1402be540b8a71546b1c5699a764a
DESKTOP-DDHE22I\주세돈 <ccmuk58@naver.com>
2017-08-12 16:04:51 +0900

    PlayerMove

---
+++ appjam/Assets/Script/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float Speed = 0;
+    public GameObject PLR;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Move(Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Move(Camera.main.ScreenToWorldPoint(Input.mousePosition), 2);
+        }
+    }
+
+    void Move(Vector3 target, int a)
+    {
+        target.z = 0;
+        if (a == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
+        }
+        if(a == 2)
+        {
+            transform.position = Vector3.Lerp(PLR.transform.position , target, 1);
+        }
+    }
+
+    void EndMove()
+    {
+
+    }
+}
